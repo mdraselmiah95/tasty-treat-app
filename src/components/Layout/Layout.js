4848623a3416d35274c6608797d65b2de516c22e
mdraselmiah95 <russelroy15@gmail.com>
2022-06-03 17:11:27 +0600

    WIP on Tasty Treat Food app

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Routers from "../../routes/Routers";
+import Footer from "../Footer/Footer";
+import Header from "../Header/Header";
+
+const Layout = () => {
+  return (
+    <div>
+      <Header />
+      <div>
+        <Routers />
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
