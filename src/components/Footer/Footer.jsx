3fb964d48f84d695bee749044e44448c1708780b
mdraselmiah95 <russelroy15@gmail.com>
2022-06-03 20:44:09 +0600

    WIP on Tasty Treat Food app

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { Container, Row, Col, ListGroup, ListGroupItem } from "reactstrap";
+import logo from "../../assets/images/res-logo.png";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <Container>
+        <Row>
+          <Col lg="3" md="4" sm="6">
+            <div className="footer__logo text-start">
+              <img src={logo} alt="logo" />
+              <h5>Tasty Treat</h5>
+            </div>
+          </Col>
+          <Col lg="3" md="4" sm="6"></Col>
+          <Col lg="3" md="4" sm="6"></Col>
+          <Col lg="3" md="4" sm="6"></Col>
+        </Row>
+      </Container>
+    </footer>
+  );
 };
 
 export default Footer;
