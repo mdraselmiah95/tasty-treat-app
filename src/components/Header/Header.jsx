5f450ddef5845f304079c6399d25fcd54d476262
mdraselmiah95 <russelroy15@gmail.com>
2022-06-03 19:35:47 +0600

    WIP on Tasty Treat Food app

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,6 +3,7 @@ import { Container } from "reactstrap";
 import { NavLink, Link } from "react-router-dom";
 
 import logo from "../../assets/images/res-logo.png";
+import "../../styles/header.css";
 
 const nav__links = [
   {
@@ -27,20 +28,45 @@ const Header = () => {
   return (
     <header className="header">
       <Container>
-        <div className="nav__wrapper">
+        <div className="nav__wrapper d-flex align-items-center justify-content-between">
+          {/* <Link to="/home"></Link> */}
           <div className="logo">
             <img src={logo} alt="logo" />
             <h5>Tasty Treat</h5>
           </div>
+
           {/* ========== menu ========== */}
           <div className="navigation">
-            <div className="menu">
+            <div className="menu d-flex align-items-center gap-5">
               {nav__links.map((item, index) => (
-                <NavLink to={item.path} key={index}>
+                <NavLink
+                  to={item.path}
+                  key={index}
+                  className={(navClass) =>
+                    navClass.isActive ? "active_menu" : ""
+                  }
+                >
                   {item.display}
                 </NavLink>
               ))}
             </div>
+          </div>
+          {/* ======== nav right icons ========= */}
+          <div className="nav__right d-flex align-items-center gap-4">
+            <span className="cart__icon">
+              <i class="ri-shopping-basket-line"></i>
+              <span className="cart__badge">2</span>
+            </span>
+
+            <span className="user">
+              <Link to="/login">
+                <i class="ri-user-line"></i>
+              </Link>
+            </span>
+
+            <span className="mobile__menu">
+              <i class="ri-menu-line"></i>
+            </span>
           </div>
         </div>
       </Container>
