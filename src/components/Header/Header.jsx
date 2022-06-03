4fb5c0ac09fc44a15f697837b87d226eabf6518a
mdraselmiah95 <russelroy15@gmail.com>
2022-06-03 19:10:18 +0600

    WIP on Tasty Treat Food app

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,51 @@
 import React from "react";
+import { Container } from "reactstrap";
+import { NavLink, Link } from "react-router-dom";
+
+import logo from "../../assets/images/res-logo.png";
+
+const nav__links = [
+  {
+    display: "Home",
+    path: "/home",
+  },
+  {
+    display: "Foods",
+    path: "/foods",
+  },
+  {
+    display: "Cart",
+    path: "/cart",
+  },
+  {
+    display: "Contact",
+    path: "/contact",
+  },
+];
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="header">
+      <Container>
+        <div className="nav__wrapper">
+          <div className="logo">
+            <img src={logo} alt="logo" />
+            <h5>Tasty Treat</h5>
+          </div>
+          {/* ========== menu ========== */}
+          <div className="navigation">
+            <div className="menu">
+              {nav__links.map((item, index) => (
+                <NavLink to={item.path} key={index}>
+                  {item.display}
+                </NavLink>
+              ))}
+            </div>
+          </div>
+        </div>
+      </Container>
+    </header>
+  );
 };
 
 export default Header;
