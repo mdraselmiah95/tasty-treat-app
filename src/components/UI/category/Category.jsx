81fc7ba440833e67707ca908936091e7834b7a4b
mdraselmiah95 <russelroy15@gmail.com>
2022-06-04 20:08:02 +0600

    WIP on Tasty Treat app UI category section

--- src/components/UI/category/Category.jsx
+++ src/components/UI/category/Category.jsx
@@ -29,7 +29,22 @@ const categoryData = [
 ];
 
 const Category = () => {
-  return <div>Category</div>;
+  return (
+    <Container>
+      <Row>
+        {categoryData.map((item, index) => (
+          <Col lg="3" md="4" sm="6" xs="6" className="mb-4" key={index}>
+            <div className="category__item d-flex align-items-center gap-3">
+              <div className="category__img">
+                <img src={item.imgUrl} alt="category__item" />
+              </div>
+              <h6>{item.display}</h6>
+            </div>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
 };
 
 export default Category;
