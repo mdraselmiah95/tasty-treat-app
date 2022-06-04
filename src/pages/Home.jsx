1a8bca3bb9221f43064c6287397760a4b75cb4c5
mdraselmiah95 <russelroy15@gmail.com>
2022-06-04 19:39:40 +0600

    WIP on Tasty Treat app

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import { Col, Container, Row } from "reactstrap";
+import Helmet from "../components/Helmet/Helmet";
+
+//images
+import heroImg from "../assets/images/hero.png";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Helmet title="Home">
+      <section>
+        <Container>
+          <Row>
+            <Col lg="6" md="6">
+              <div className="hero__content">
+                <h5 className="mb-3">Easy way to make an order</h5>
+                <h1 className="mb-4 hero__title">
+                  <span>HUNGRY?</span> Just wait <br /> food at
+                  <span> your door</span>
+                </h1>
+                <p>
+                  We are passionate about food and are always prompt to deliver
+                  whenever the radar blips hungry.
+                </p>
+              </div>
+            </Col>
+            <Col lg="6" md="6">
+              <div className="hero__img">
+                <img src={heroImg} alt="hero-img" className="w-100" />
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
 };
 
 export default Home;
