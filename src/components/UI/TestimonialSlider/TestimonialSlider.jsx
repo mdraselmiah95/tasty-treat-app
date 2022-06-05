aa276437b4599958f7e03e71b9ca7a030d6be873
mdraselmiah95 <russelroy15@gmail.com>
2022-06-05 18:36:17 +0600

    WIP on Tasty Treat Food App Testimonial section

---
+++ src/components/UI/TestimonialSlider/TestimonialSlider.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Slider from "react-slick";
+
+const TestimonialSlider = () => {
+  return <Slider>TestimonialSlider</Slider>;
+};
+
+export default TestimonialSlider;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -325,6 +325,22 @@ const Home = () => {
         <Container>
           <Row>
             <Col lg="6" md="6">
+              <div className="testimonial ">
+                <h5 className="testimonial__subtitle mb-4">Testimonial</h5>
+                <h2 className="testimonial__title mb-4">
+                  What our <span>customers</span> are saying
+                </h2>
+                <p className="testimonial__desc">
+                  At Testy Treat, customer satisfaction is very important to us.
+                  Peek into our latest survey and see what our customers are
+                  saying about us.
+                </p>
+
+                {/* <TestimonialSlider /> */}
+              </div>
+            </Col>
+
+            <Col lg="6" md="6">
               <img src={networkImg} alt="testimonial-img" className="w-100" />
             </Col>
           </Row>
