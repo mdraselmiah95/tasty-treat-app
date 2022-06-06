0e26e0e65fb18b6c34a185c8b42e8418270180db
mdraselmiah95 <russelroy15@gmail.com>
2022-06-06 17:14:08 +0600

    WIP on Tasty Treat cart part

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,12 +3,16 @@ import Routers from "../../routes/Routers";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 import Carts from "../UI/cart/Carts";
+import { useSelector } from "react-redux";
 
 const Layout = () => {
+  const showCart = useSelector((state) => state.cartUi.cartIsVisible);
+
   return (
     <div>
       <Header />
-      <Carts />
+      {showCart && <Carts />}
+
       <div>
         <Routers />
       </div>
