6b2d9cb806e36f5cbe61c46d0dd069179fafbd8b
mdraselmiah95 <russelroy15@gmail.com>
2022-06-06 16:29:44 +0600

    WIP on Tasty Treat cart part

--- src/components/UI/cart/Carts.jsx
+++ src/components/UI/cart/Carts.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import { ListGroup } from "reactstrap";
+import { Link } from "react-router-dom";
+import CartItem from "./CartItem";
 
 const Carts = () => {
   return (
     <div className="cart__container">
-      <ListGroup>
-        <h2>hi</h2>
+      <ListGroup className="cart">
+        <div className="cart__close">
+          <span>
+            <i className="ri-close-fill"></i>
+          </span>
+        </div>
+        <div className="cart__item-list">
+          <CartItem />
+        </div>
       </ListGroup>
     </div>
   );
