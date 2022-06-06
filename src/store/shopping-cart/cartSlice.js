80da6ee429f385e57114cd99e15c8552395f5d0b
mdraselmiah95 <russelroy15@gmail.com>
2022-06-06 15:09:40 +0600

    WIP on Tasty Treat creating Store by Redux toolkit

---
+++ src/store/shopping-cart/cartSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  cartItems: [],
+  totalQuantity: 0,
+  totalAmount: 0,
+};
+
+const cartSlice = {
+  name: "cart",
+  initialState,
+
+  reducers: {
+    addItem(state, action) {
+      const newItem = action.payload;
+      const existingItem = state.cartItems.find(
+        (item) => item.id === newItem.id
+      );
+      state.totalQuantity++;
+    },
+  },
+};
