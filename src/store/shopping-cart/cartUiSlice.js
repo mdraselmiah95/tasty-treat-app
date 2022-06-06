6705e127d3d15bd025e3105744a3af3edf576ded
mdraselmiah95 <russelroy15@gmail.com>
2022-06-06 17:01:42 +0600

    WIP on Tasty Treat cart part

---
+++ src/store/shopping-cart/cartUiSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartUiSlice = createSlice({
+  name: "cartUi",
+  initialState: { cartIsVisible: false },
+
+  reducers: {
+    toggle(state) {
+      state.cartIsVisible = !state.cartIsVisible;
+    },
+  },
+});
+
+export const cartAction = cartUiSlice.actions;
+export default cartUiSlice;
