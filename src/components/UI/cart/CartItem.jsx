4a089b3696dbeb1bd4c001749ff2081837d1c598
mdraselmiah95 <russelroy15@gmail.com>
2022-06-06 16:25:12 +0600

    WIP on Tasty Treat cart part

---
+++ src/components/UI/cart/CartItem.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { ListGroup } from "reactstrap";
+
+import image from "../../../assets/images/product_01.1.jpg";
+
+const CartItem = () => {
+  return (
+    <ListGroup className="border-0 cart__item">
+      <div className="cart__item-info d-flex gap-2">
+        <img src={image} alt="product-img" />
+        <div className="cart__product-info w-100 d-flex align-items-center gap-4 justify-content-between">
+          <div>
+            <h6 className="cart__product-title">Burger</h6>
+            <p className=" d-flex align-items-center gap-5 cart__product-price">
+              {/* {quantity}x <span>${totalPrice}</span> */}
+              24
+            </p>
+            <div className=" d-flex align-items-center justify-content-between increase__decrease-btn">
+              <span className="increase__btn">
+                <i className="ri-add-line"></i>
+              </span>
+              <span className="quantity">2</span>
+              <span className="decrease__btn">
+                <i className="ri-subtract-line"></i>
+              </span>
+            </div>
+          </div>
+          <span className="delete__btn">
+            <i className="ri-close-line"></i>
+          </span>
+        </div>
+      </div>
+    </ListGroup>
+  );
+};
+
+export default CartItem;
---
+++ src/components/UI/cart/Carts.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { ListGroup } from "reactstrap";
+
+const Carts = () => {
+  return (
+    <div className="cart__container">
+      <ListGroup>
+        <h2>hi</h2>
+      </ListGroup>
+    </div>
+  );
+};
+
+export default Carts;
