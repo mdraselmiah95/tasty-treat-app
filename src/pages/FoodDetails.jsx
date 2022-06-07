f31eeb5ddfa10f38dc26078ec1eff6d3280f671b
mdraselmiah95 <russelroy15@gmail.com>
2022-06-07 18:27:25 +0600

    WIP on Tasty Treat Product Details

--- src/pages/FoodDetails.jsx
+++ src/pages/FoodDetails.jsx
@@ -68,10 +68,18 @@ const FoodDetails = () => {
 
             <Col lg="12">
               <div className="tabs d-flex align-items-center gap-5 py-3">
-                <h6 className="tab-active" onClick={() => setTab("desc")}>
+                <h6
+                  className={`${tab === "desc" ? "tab__active" : ""}`}
+                  onClick={() => setTab("desc")}
+                >
                   Description
                 </h6>
-                <h6 onClick={() => setTab("rev")}>Review</h6>
+                <h6
+                  className={`${tab === "rev" ? "tab__active" : ""}`}
+                  onClick={() => setTab("rev")}
+                >
+                  Review
+                </h6>
               </div>
 
               {tab === "desc" ? (
