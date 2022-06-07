5349fb9b0d09debca2b8ca5efa2447f695469ac7
mdraselmiah95 <russelroy15@gmail.com>
2022-06-07 14:01:02 +0600

    WIP on Tasty Treat All Foods page

--- src/components/Helmet/Helmet.js
+++ src/components/Helmet/Helmet.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Helmet = (props) => {
-  document.title = "Tasty Food -" + props.title;
+  document.title = "Tasty Food - " + props.title;
   return <div className="w-100">{props.children}</div>;
 };
 
--- src/pages/AllFoods.jsx
+++ src/pages/AllFoods.jsx
@@ -1,11 +1,43 @@
 import React from "react";
+import { Container, Row, Col } from "reactstrap";
 import Helmet from "../components/Helmet/Helmet";
 import CommonSection from "../components/UI/common-section/CommonSection";
 
 const AllFoods = () => {
   return (
-    <Helmet title="All-Foods">
+    <Helmet title="All Foods">
       <CommonSection title="All Foods" />
+
+      <section>
+        <Container>
+          <Row>
+            <Col lg="6" md="6" sm="6" xs="12">
+              <div className="search__widget d-flex align-items-center justify-content-between ">
+                <input
+                  type="text"
+                  placeholder="I'm looking for...."
+                  // value={searchTerm}
+                  // onChange={(e) => setSearchTerm(e.target.value)}
+                />
+                <span>
+                  <i className="ri-search-line"></i>
+                </span>
+              </div>
+            </Col>
+            <Col lg="6" md="6" sm="6" xs="12" className="mb-5">
+              <div className="sorting__widget text-end">
+                <select className="w-50">
+                  <option>Default</option>
+                  <option value="ascending">Alphabetically, A-Z</option>
+                  <option value="descending">Alphabetically, Z-A</option>
+                  <option value="high-price">High Price</option>
+                  <option value="low-price">Low Price</option>
+                </select>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
     </Helmet>
   );
 };
