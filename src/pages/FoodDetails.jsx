bbd8d008c8608e335813403db8d482b86866d4fb
mdraselmiah95 <russelroy15@gmail.com>
2022-06-07 17:37:41 +0600

    WIP on Tasty Treat Product Details

--- src/pages/FoodDetails.jsx
+++ src/pages/FoodDetails.jsx
@@ -54,8 +54,74 @@ const FoodDetails = () => {
                 <p className="category mb-5">
                   Category: <span>3</span>
                 </p>
-
                 <button className="addTOCart__btn">Add to Cart</button>
+              </div>
+            </Col>
+
+            <Col lg="12">
+              <div className="tabs d-flex align-items-center gap-5 py-3">
+                <h6>Description</h6>
+                <h6>Review</h6>
+              </div>
+
+              <div className="tab__content">
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Voluptatum veritatis quibusdam rerum.
+                </p>
+              </div>
+
+              <div className="tab__form mb-3">
+                <div className="review pt-5">
+                  <p className="user__name mb-0">Jhon Doe</p>
+                  <p className="user__email">jhon1@gmail.com</p>
+                  <p className="feedback__text">great product</p>
+                </div>
+
+                <div className="review">
+                  <p className="user__name mb-0">Jhon Doe</p>
+                  <p className="user__email">jhon1@gmail.com</p>
+                  <p className="feedback__text">great product</p>
+                </div>
+
+                <div className="review">
+                  <p className="user__name mb-0">Jhon Doe</p>
+                  <p className="user__email">jhon1@gmail.com</p>
+                  <p className="feedback__text">great product</p>
+                </div>
+                <form className="form">
+                  <div className="form__group">
+                    <input
+                      type="text"
+                      placeholder="Enter your name"
+                      // onChange={(e) => setEnteredName(e.target.value)}
+                      required
+                    />
+                  </div>
+
+                  <div className="form__group">
+                    <input
+                      type="text"
+                      placeholder="Enter your email"
+                      // onChange={(e) => setEnteredEmail(e.target.value)}
+                      required
+                    />
+                  </div>
+
+                  <div className="form__group">
+                    <textarea
+                      rows={5}
+                      type="text"
+                      placeholder="Write your review"
+                      // onChange={(e) => setReviewMsg(e.target.value)}
+                      required
+                    />
+                  </div>
+
+                  <button type="submit" className="addTOCart__btn">
+                    Submit
+                  </button>
+                </form>
               </div>
             </Col>
           </Row>
