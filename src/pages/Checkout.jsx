022906a974abc6b4e2484a5bd37f765033af046b
mdraselmiah95 <russelroy15@gmail.com>
2022-06-08 21:25:59 +0600

    WIP on Tasty Treat Cart page

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,7 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { Container, Row, Col } from "reactstrap";
+import Helmet from "../components/Helmet/Helmet";
+import CommonSection from "../components/UI/common-section/CommonSection";
 
 const Checkout = () => {
-  return <div>Checkout</div>;
+  return (
+    <Helmet title="Checkout">
+      <CommonSection title="Checkout" />
+
+      <section>
+        <Container>
+          <Row>
+            <Col lg="8" md="6">
+              <h6 className="mb-4">Shipping Address</h6>
+
+              <form className="checkout__form">
+                <div className="form__group">
+                  <input
+                    type="text"
+                    placeholder="Enter your name"
+                    required
+                    // onChange={(e) => setEnterName(e.target.value)}
+                  />
+                </div>
+
+                <div className="form__group">
+                  <input
+                    type="email"
+                    placeholder="Enter your email"
+                    required
+                    // onChange={(e) => setEnterEmail(e.target.value)}
+                  />
+                </div>
+                <div className="form__group">
+                  <input
+                    type="number"
+                    placeholder="Phone number"
+                    required
+                    // onChange={(e) => setEnterNumber(e.target.value)}
+                  />
+                </div>
+                <div className="form__group">
+                  <input
+                    type="text"
+                    placeholder="Country"
+                    required
+                    // onChange={(e) => setEnterCountry(e.target.value)}
+                  />
+                </div>
+                <div className="form__group">
+                  <input
+                    type="text"
+                    placeholder="City"
+                    required
+                    // onChange={(e) => setEnterCity(e.target.value)}
+                  />
+                </div>
+                <div className="form__group">
+                  <input
+                    type="number"
+                    placeholder="Postal code"
+                    required
+                    // onChange={(e) => setPostalCode(e.target.value)}
+                  />
+                </div>
+                <button type="submit" className="addTOCart__btn">
+                  Payment
+                </button>
+              </form>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
 };
 
 export default Checkout;
