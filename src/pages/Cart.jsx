45f1743a98fad98d38be9134d2cb69e608cc482b
mdraselmiah95 <russelroy15@gmail.com>
2022-06-08 20:45:25 +0600

    WIP on Tasty Treat page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Container, Row, Col } from "reactstrap";
+import Helmet from "../components/Helmet/Helmet";
+import CommonSection from "../components/UI/common-section/CommonSection";
 
 const Cart = () => {
-  return <div>Cart</div>;
+  return (
+    <Helmet title="Cart">
+      <CommonSection title="Your Cart" />
+    </Helmet>
+  );
 };
 
 export default Cart;
