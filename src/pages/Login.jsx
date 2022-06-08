ec11bd2d45ae5310dad68db35b55e368f6d07357
mdraselmiah95 <russelroy15@gmail.com>
2022-06-08 22:54:31 +0600

    WIP on Tasty Treat Logint page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import { Link } from "react-router-dom";
+import Helmet from "../components/Helmet/Helmet";
+import CommonSection from "../components/UI/common-section/CommonSection";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <Helmet title="Login">
+      <CommonSection title="Login" />
+    </Helmet>
+  );
 };
 
 export default Login;
