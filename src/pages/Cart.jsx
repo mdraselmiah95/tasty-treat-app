07c12073e7ef9b9018c5d74e9941bbd88a3ec4f3
mdraselmiah95 <russelroy15@gmail.com>
2022-06-08 20:54:54 +0600

    WIP on Tasty Treat page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -4,6 +4,9 @@ import { Container, Row, Col } from "reactstrap";
 import Helmet from "../components/Helmet/Helmet";
 import CommonSection from "../components/UI/common-section/CommonSection";
 
+//Style CSS
+import "../styles/cart-page.css";
+
 const Cart = () => {
   return (
     <Helmet title="Cart">
@@ -23,6 +26,7 @@ const Cart = () => {
                   </tr>
                 </thead>
                 <tbody>
+                  <Tr />
                   {/* {cartItems.map((item) => (
                     <Tr item={item} key={item.id} />
                   ))} */}
@@ -33,6 +37,29 @@ const Cart = () => {
         </Container>
       </section>
     </Helmet>
+  );
+};
+
+const Tr = (props) => {
+  // const { id, image01, title, price, quantity } = props.item;
+  // const dispatch = useDispatch();
+
+  // const deleteItem = () => {
+  //   dispatch(cartActions.deleteItem(id));
+  // };
+  return (
+    <tr>
+      <td className="text-center cart__img-box">
+        {/* <img src={image01} alt="cart-img" /> */}
+        img
+      </td>
+      <td className="text-center">tt</td>
+      <td className="text-center">$p</td>
+      <td className="text-center">px</td>
+      <td className="text-center cart__item-del">
+        <i className="ri-delete-bin-line"></i>
+      </td>
+    </tr>
   );
 };
 
