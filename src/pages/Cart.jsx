0d47c81f68a3d443dd36113e15d2ea4bfd08acbd
mdraselmiah95 <russelroy15@gmail.com>
2022-06-08 21:00:58 +0600

    WIP on Tasty Treat page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -8,6 +8,7 @@ import CommonSection from "../components/UI/common-section/CommonSection";
 import "../styles/cart-page.css";
 
 const Cart = () => {
+  const cartItems = useSelector((state) => state.cart.cartItem);
   return (
     <Helmet title="Cart">
       <CommonSection title="Your Cart" />
@@ -15,23 +16,27 @@ const Cart = () => {
         <Container>
           <Row>
             <Col lg="12">
-              <table className="table table-bordered">
-                <thead>
-                  <tr>
-                    <th>Image</th>
-                    <th>Product Title</th>
-                    <th>Price</th>
-                    <th>Quantity</th>
-                    <th>Delete</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <Tr />
-                  {/* {cartItems.map((item) => (
-                    <Tr item={item} key={item.id} />
-                  ))} */}
-                </tbody>
-              </table>
+              {cartItems.length === 0 ? (
+                <h5 className="text-center">Your cart is empty</h5>
+              ) : (
+                <table className="table table-bordered">
+                  <thead>
+                    <tr>
+                      <th>Image</th>
+                      <th>Product Title</th>
+                      <th>Price</th>
+                      <th>Quantity</th>
+                      <th>Delete</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <Tr />
+                    {cartItems.map((item) => (
+                      <Tr item={item} key={item.id} />
+                    ))}
+                  </tbody>
+                </table>
+              )}
             </Col>
           </Row>
         </Container>
@@ -41,7 +46,7 @@ const Cart = () => {
 };
 
 const Tr = (props) => {
-  // const { id, image01, title, price, quantity } = props.item;
+  const { id, image01, title, price, quantity } = props.item;
   // const dispatch = useDispatch();
 
   // const deleteItem = () => {
@@ -50,12 +55,12 @@ const Tr = (props) => {
   return (
     <tr>
       <td className="text-center cart__img-box">
-        {/* <img src={image01} alt="cart-img" /> */}
+        <img src={image01} alt="cart-img" />
         img
       </td>
-      <td className="text-center">tt</td>
-      <td className="text-center">$p</td>
-      <td className="text-center">px</td>
+      <td className="text-center">{title}</td>
+      <td className="text-center">${price}</td>
+      <td className="text-center">{quantity}px</td>
       <td className="text-center cart__item-del">
         <i className="ri-delete-bin-line"></i>
       </td>
